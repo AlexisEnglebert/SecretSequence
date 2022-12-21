02342112db9173f0e04fbf5bb78ae816d1684f25
alexisloic21 <englebert.alexis@gmail.com>
2022-12-21 19:23:45 +0100

    src: Add window and OpenGL context

---
+++ CMakeFiles/SecretSequence.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SecretSequence.dir/src/main.cpp.o"
+  "CMakeFiles/SecretSequence.dir/src/main.cpp.o.d"
+  "SecretSequence"
+  "SecretSequence.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SecretSequence.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <iostream>
+
+// Don't switch these two, because glfw3 detect that glad is included 
+#define GLFW_INCLUDE_NONE
+
+#include <glad/glad.h> 
+#include <GLFW/glfw3.h>
+
+
+typedef struct{
+	GLFWwindow* window; //store our window
+}core_t;
+
+static core_t core;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+	// Update viewport    
+    glViewport(0, 0, width, height);
+}
+
+unsigned int create_window(){
+
+	core.window = glfwCreateWindow(640, 480, "Secret Sequence", NULL, NULL);
+	if(core.window == NULL){
+		std::cerr << "Failed to create window" << std::endl;
+		return -1;
+	}
+
+	glfwMakeContextCurrent(core.window);
+    glfwSetFramebufferSizeCallback(core.window, framebuffer_size_callback);
+    
+    return 1;
+}
+
+unsigned int init_opengl(){
+
+	if(gladLoadGLLoader((GLADloadproc)glfwGetProcAddress) == 0){
+        std::cerr << "Failed to initialize Opengl context" << std::endl;
+        return -1;
+    }
+
+    return 1;
+}
+
+
+void Render(){
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(core.window);
+}
+
+int main(int argc, char ** argv){
+	printf("%s \n", "Hello World !");
+
+	if(!glfwInit()){
+		std::cerr << "Failed to initialize GLFW" << std::endl;
+		return -1;
+	}
+	
+	create_window();
+	init_opengl();
+
+	while (!glfwWindowShouldClose(core.window)) {
+		Render();
+		
+		glfwPollEvents();
+	}
+
+
+	glfwDestroyWindow(core.window);
+	glfwTerminate();
+
+}
